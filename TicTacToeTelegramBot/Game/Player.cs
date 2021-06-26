6739e6d5b04b36058a02274f83f3a8377b10a229
DenisAliev <aliev372001@gmail.com>
2021-06-26 18:29:39 +0300

    Changed Player

--- TicTacToeTelegramBot/Game/Player.cs
+++ TicTacToeTelegramBot/Game/Player.cs
@@ -1,7 +1,11 @@
+using Telegram.Bot.Types;
+
 namespace TicTacToeTelegramBot.Game
 {
     public class Player
     {
-        
+        public long Id { set; get; }
+        public ChatId ChatId { set; get; }
+        public string Name { set; get; }
     }
 }
